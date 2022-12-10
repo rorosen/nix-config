8f700ae6cd56f81484117dbe1ddd7f04f2847dce
Robert Rose <robert.rose@mailbox.org>
2022-12-10 22:24:03 +0100

    feat(i3): add xf86 keybindings

--- configuration/user/modules/i3/i3.nix
+++ configuration/user/modules/i3/i3.nix
@@ -15,15 +15,21 @@
       terminal = "${pkgs.alacritty}/bin/alacritty";
 
       keybindings = lib.mkOptionDefault {
+        # Use selected XF86 keyboard symbols
+        "XF86AudioPause" = "exec ${pkgs.playerctl}/bin/playerctl play-pause -p spotify";
+        "XF86AudioPlay" = "exec ${pkgs.playerctl}/bin/playerctl play-pause -p spotify";
+        "XF86AudioPrev" = "exec ${pkgs.playerctl}/bin/playerctl previous -p spotify";
+        "XF86AudioNext" = "exec ${pkgs.playerctl}/bin/playerctl next -p spotify";
         "XF86AudioMute" = "exec amixer set Master toggle";
         "XF86AudioRaiseVolume" = "exec amixer set Master 4%+";
         "XF86AudioLowerVolume" = "exec amixer set Master 4%-";
         "XF86MonBrightnessDown" = "exec brightnessctl set 4%-";
         "XF86MonBrightnessUp" = "exec brightnessctl set 4%+";
+        # Custom keybindings
         "${modifier}+d" = "exec rofi -no-config -show drun -theme ~/.config/rofi/config.rasi";
         "${modifier}+x" = "exec bash $HOME/.config/polybar/sysmenu.sh";
         "${modifier}+n" = "exec nemo";
-        #"${modifier}+Shift+x" = "exec systemctl suspend";
+        # Keybindings for additional workaspces
         "${modifier}+Ctrl+1" = "workspace number 11";
         "${modifier}+Ctrl+2" = "workspace number 12";
         "${modifier}+Ctrl+3" = "workspace number 13";
@@ -34,6 +40,16 @@
         "${modifier}+Ctrl+8" = "workspace number 18";
         "${modifier}+Ctrl+9" = "workspace number 19";
         "${modifier}+Ctrl+0" = "workspace number 20";
+        "${modifier}+Shift+Ctrl+1" = "move container to workspace number 11";
+        "${modifier}+Shift+Ctrl+2" = "move container to workspace number 12";
+        "${modifier}+Shift+Ctrl+3" = "move container to workspace number 13";
+        "${modifier}+Shift+Ctrl+4" = "move container to workspace number 14";
+        "${modifier}+Shift+Ctrl+5" = "move container to workspace number 15";
+        "${modifier}+Shift+Ctrl+6" = "move container to workspace number 16";
+        "${modifier}+Shift+Ctrl+7" = "move container to workspace number 17";
+        "${modifier}+Shift+Ctrl+8" = "move container to workspace number 18";
+        "${modifier}+Shift+Ctrl+9" = "move container to workspace number 19";
+        "${modifier}+Shift+Ctrl+0" = "move container to workspace number 20";
       };
 
       window.titlebar = false;
