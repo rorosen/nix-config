eb4bead41b1bb23be58da1cdf110e5d459472de5
Robert Rose <robert.rose@mailbox.org>
2023-04-13 21:05:11 +0200

    refactor: use flake based setup

---
+++ home-manager/home.nix
@@ -0,0 +1,42 @@
+{ config, pkgs, ... }:
+
+{
+  home = {
+    username = "rob";
+    homeDirectory = "/home/rob";
+    keyboard.layout = "de";
+
+    sessionVariables = {
+      RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+    };
+  };
+
+  programs.home-manager.enable = true;
+
+  xsession = {
+    enable = true;
+    numlock.enable = true;
+  };
+
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.11";
+
+  imports = [
+    ./packages.nix
+    ./i3
+    ./polybar
+    ./vscode
+    ./rofi
+    ./zsh
+    ./alacritty.nix
+    ./gtk.nix
+    ./qt.nix
+    ./gnome-keyring.nix
+    ./git.nix
+    ./ssh-agent.nix
+    ./nemo.nix
+    ./blueman.nix
+  ];
+}
