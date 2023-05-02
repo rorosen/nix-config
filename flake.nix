0190d69f02d1286edc1bf9794fb6b9fc9732f0c4
Robert Rose <robert.rose@secunet.com>
2023-05-02 18:37:09 +0200

    feat: add option for waybar hwmon-path

--- flake.nix
+++ flake.nix
@@ -62,6 +62,7 @@
               ({ pkgs, ... }: {
                 home.username = "rob";
                 home.homeDirectory = "/home/rob";
+                programs.waybar.hwmon-path = "/sys/devices/platform/thinkpad_hwmon/hwmon/hwmon4/temp1_input";
                 wayland.windowManager.sway.startupSync = [
                   {
                     command = "${pkgs.ungoogled-chromium}/bin/chromium";
--- home-manager/waybar/default.nix
+++ home-manager/waybar/default.nix
@@ -1,111 +1,124 @@
-{ pkgs, ... }:
+{ pkgs, lib, config, ... }:
 
+let
+  cfg = config.programs.waybar;
+in
 {
-  programs.waybar = {
-    enable = true;
+  options.programs.waybar.hwmon-path = lib.mkOption {
+    type = lib.types.str;
+    default = "";
+    description = "The temperature path to use, e.g. /sys/class/hwmon/hwmon2/temp1_input instead of one in /sys/class/thermal/.";
+  };
 
-    settings = [
-      {
-        name = "top-bar";
-        layer = "top";
-        position = "top";
-        height = 30;
-        spacing = 8;
-        modules-left = [ "custom/launcher" "sway/workspaces" "sway/mode" "sway/scratchpad" ];
-        modules-center = [ "sway/window" ];
-        modules-right = [ "sway/language" "idle_inhibitor" "pulseaudio" "backlight" "battery" "tray" ];
-        tray.spacing = 10;
-        idle_inhibitor = {
-          format = "{icon}";
-          format-icons = {
-            activated = " ";
-            deactivated = " ";
-          };
-        };
-        pulseaudio = {
-          format = "{volume}% {icon}  {format_source}";
-          format-bluetooth = "{volume}% {icon}   {format_source}";
-          format-bluetooth-muted = "   {icon}   {format_source}";
-          format-muted = "   {format_source}";
-          format-source = "{volume}%  ";
-          format-source-muted = " ";
-          format-icons = {
-            headphone = " ";
-            hands-free = " ";
-            headset = " ";
-            phone = " ";
-            portable = " ";
-            car = " ";
-            default = [ " " " " " " ];
-          };
-          on-click = "${pkgs.pavucontrol}/bin/pavucontrol";
-        };
-        battery = {
-          states.critical = 10;
-          format = "{capacity}% {icon}";
-          format-charging = "{capacity}% {icon}";
-          format-plugged = "{capacity}%  ";
-          format-alt = "{time} {icon}";
-          format-icons = [ " " " " " " " " " " ];
-        };
-        backlight = {
-          format = "{percent}% {icon}";
-          format-icons = [ " " " " " " " " " " " " " " " " " " ];
-        };
-        "custom/launcher" = {
-          on-click = "${pkgs.wofi}/bin/wofi --show=drun --allow-images --insensitive";
-          format = " ";
-          tooltip = false;
-        };
-      }
-      {
-        name = "bottom-bar";
-        layer = "top";
-        position = "bottom";
-        height = 30;
-        spacing = 8;
-        modules-left = [ "custom/powermenu" "cpu" "memory" "disk" "temperature" ];
-        modules-right = [ "network" "clock" ];
-        cpu.format = "  {usage}%";
-        disk.format = "  {percentage_used}%";
-        memory = {
-          format = "  {}%";
-          tooltip-format = "{used:0.1f}GiB used out of {total}";
-        };
-        temperature = {
-          critical-threshold = 80;
-          format = "{icon} {temperatureC}°C";
-          format-icons = [ " " " " " " " " " " ];
-        };
-        clock = {
-          format = "{:%H:%M}  ";
-          tooltip-format = "<big>{:%Y %B}</big>\n<tt><big>{calendar}</big></tt>";
-          calendar-weeks-pos = "right";
-          today-format = "<span color = '#ff6699'><u>{}</u></span>";
-          format-calendar = "<span color='#ecc6d9'>{}</span>";
-          format-calendar-weeks = "<span color='#99ffdd'>KW{:%V}</span>";
-          format-calendar-weekdays = "<span color='#ffcc66'>{}</span>";
-        };
-        network = {
-          interval = 1;
-          min-length = 30;
-          max-length = 30;
-          format-wifi = "  {essid}     {bandwidthDownBytes}    {bandwidthUpBytes}";
-          format-ethernet = "      {bandwidthDownBytes}    {bandwidthUpBytes}";
-          tooltip-format = "{ifname}: {ipaddr}/{cidr} via {gwaddr}";
-          tooltip-format-wifi = "{ifname}: {ipaddr}/{cidr} via {gwaddr} ({signalStrength}%)";
-          format-linked = "{ifname} (No IP)  ";
-          format-disconnected = "Disconnected ⚠ ";
-          on-click = "${pkgs.networkmanagerapplet}/bin/nm-connection-editor";
-        };
-        "custom/powermenu" = {
-          on-click = "${pkgs.wlogout}/bin/wlogout";
-          format = " ";
-          tooltip = false;
-        };
-      }
-    ];
+  config = {
+    programs.waybar = {
+      enable = true;
 
-    style = builtins.readFile ./style.css;
+      settings = [
+        {
+          name = "top-bar";
+          layer = "top";
+          position = "top";
+          height = 30;
+          spacing = 8;
+          modules-left = [ "custom/launcher" "sway/workspaces" "sway/mode" "sway/scratchpad" ];
+          modules-center = [ "sway/window" ];
+          modules-right = [ "sway/language" "idle_inhibitor" "pulseaudio" "backlight" "battery" "tray" ];
+          tray.spacing = 10;
+          idle_inhibitor = {
+            format = "{icon}";
+            format-icons = {
+              activated = " ";
+              deactivated = " ";
+            };
+          };
+          pulseaudio = {
+            format = "{volume}% {icon}  {format_source}";
+            format-bluetooth = "{volume}% {icon}   {format_source}";
+            format-bluetooth-muted = "   {icon}   {format_source}";
+            format-muted = "   {format_source}";
+            format-source = "{volume}%  ";
+            format-source-muted = " ";
+            format-icons = {
+              headphone = " ";
+              hands-free = " ";
+              headset = " ";
+              phone = " ";
+              portable = " ";
+              car = " ";
+              default = [ " " " " " " ];
+            };
+            on-click = "${pkgs.pavucontrol}/bin/pavucontrol";
+          };
+          battery = {
+            states.critical = 10;
+            format = "{capacity}% {icon}";
+            format-charging = "{capacity}% {icon}";
+            format-plugged = "{capacity}%  ";
+            format-alt = "{time} {icon}";
+            format-icons = [ " " " " " " " " " " ];
+          };
+          backlight = {
+            format = "{percent}% {icon}";
+            format-icons = [ " " " " " " " " " " " " " " " " " " ];
+          };
+          "custom/launcher" = {
+            on-click = "${pkgs.wofi}/bin/wofi --show=drun --allow-images --insensitive";
+            format = " ";
+            tooltip = false;
+          };
+        }
+        {
+          name = "bottom-bar";
+          layer = "top";
+          position = "bottom";
+          height = 30;
+          spacing = 8;
+          modules-left = [ "custom/powermenu" "cpu" "memory" "disk" "temperature" ];
+          modules-right = [ "network" "clock" ];
+          cpu.format = "  {usage}%";
+          disk.format = "  {percentage_used}%";
+          memory = {
+            format = "  {}%";
+            tooltip-format = "{used:0.1f}GiB used out of {total}";
+          };
+          temperature = {
+            critical-threshold = 80;
+            format = "{icon} {temperatureC}°C";
+            format-icons = [ " " " " " " " " " " ];
+            hwmon-path = lib.mkIf (cfg.hwmon-path != "") cfg.hwmon-path;
+            interval = 5;
+          };
+          clock = {
+            format = "{:%H:%M}  ";
+            tooltip-format = "<big>{:%Y %B}</big>\n<tt><big>{calendar}</big></tt>";
+            calendar-weeks-pos = "right";
+            today-format = "<span color = '#ff6699'><u>{}</u></span>";
+            format-calendar = "<span color='#ecc6d9'>{}</span>";
+            format-calendar-weeks = "<span color='#99ffdd'>KW{:%V}</span>";
+            format-calendar-weekdays = "<span color='#ffcc66'>{}</span>";
+          };
+          network = {
+            interval = 1;
+            min-length = 30;
+            max-length = 30;
+            format-wifi = "  {essid}     {bandwidthDownBytes}    {bandwidthUpBytes}";
+            format-ethernet = "      {bandwidthDownBytes}    {bandwidthUpBytes}";
+            tooltip-format = "{ifname}: {ipaddr}/{cidr} via {gwaddr}";
+            tooltip-format-wifi = "{ifname}: {ipaddr}/{cidr} via {gwaddr} ({signalStrength}%)";
+            format-linked = "{ifname} (No IP)  ";
+            format-disconnected = "Disconnected ⚠ ";
+            on-click = "${pkgs.networkmanagerapplet}/bin/nm-connection-editor";
+          };
+          "custom/powermenu" = {
+            on-click = "${pkgs.wlogout}/bin/wlogout";
+            format = " ";
+            tooltip = false;
+          };
+        }
+      ];
+
+      style = builtins.readFile ./style.css;
+    };
   };
 }
