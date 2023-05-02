50c84244fa8416c68e50dcaff0b29f6fbb164a2a
Robert Rose <robert.rose@secune.com>
2023-05-02 14:21:39 +0200

    chore: remove comment from config file

--- nixos/hp-configuration.nix
+++ nixos/hp-configuration.nix
@@ -7,15 +7,13 @@
   ];
 
   system.flavour = "sway";
-
   networking.hostName = "hp";
-  boot.initrd.kernelModules = [ "amdgpu" ];
 
+  boot.initrd.kernelModules = [ "amdgpu" ];
   # Enable scanner support
   hardware.sane = {
     enable = true;
     extraBackends = [ pkgs.sane-airscan ];
   };
-
   users.users.rob.extraGroups = [ "scanner" ];
 }
