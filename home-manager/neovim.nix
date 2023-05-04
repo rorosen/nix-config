821f6689963a7f359d012cce7a16c3fb40d28c6e
Robert Rose <robert.rose@mailbox.org>
2023-05-04 23:40:51 +0200

    feat: add rust analyzer to neovim

--- home-manager/neovim.nix
+++ home-manager/neovim.nix
@@ -1,15 +1,26 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.neovim = {
     enable = true;
-    defaultEditor = true;
-
     viAlias = true;
     vimAlias = true;
+    defaultEditor = true;
+
+    extraPackages = with pkgs; [
+      rust-analyzer
+    ];
+
+    plugins = with pkgs.vimPlugins; [
+      nvim-lspconfig
+    ];
 
     extraConfig = ''
       set number relativenumber
+    '';
+
+    extraLuaConfig = ''
+      require'lspconfig'.rust_analyzer.setup{}
     '';
   };
 }
