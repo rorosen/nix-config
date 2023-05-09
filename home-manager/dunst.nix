ab66526bb4426fa569ec336e71bb902e43c2dafe
Robert Rose <robert.rose@mailbox.org>
2023-05-09 22:57:51 +0200

    chore(dunst): change settings

--- home-manager/dunst.nix
+++ home-manager/dunst.nix
@@ -5,19 +5,22 @@
     enable = true;
     settings = {
       global = {
+        follow = "mouse";
         width = 300;
         height = 300;
         offset = "30x50";
         origin = "top-right";
         transparency = 10;
-        frame_color = "#eceff1";
+        frame_width = 1;
+        frame_color = "#c9545d";
+        corner_radius = 10;
         font = "Droid Sans 9";
       };
 
       urgency_normal = {
         background = "#37474f";
         foreground = "#eceff1";
-        timeout = 10;
+        timeout = 5;
       };
     };
   };
