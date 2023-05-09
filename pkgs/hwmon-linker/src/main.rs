0698d05d4948bb047588ffb4993cbf603dcab8f4
Robert Rose <robert.rose@mailbox.org>
2023-05-09 22:33:13 +0200

    feat: link hwmon path

--- pkgs/hwmon-linker/src/main.rs
+++ pkgs/hwmon-linker/src/main.rs
@@ -1,28 +1,66 @@
-use std::{fs, io};
+use std::{fs, io, os::unix::fs::symlink};
 
-use regex::Regex;
+use clap::Parser;
 use walkdir::{DirEntry, WalkDir};
 
 const HWMON_DIR: &str = "/sys/class/hwmon";
+const PATTERN_TEMP: &str = "temp";
+const PATTERN_INPUT: &str = "_input";
+const PATTERN_LABEL: &str = "_label";
+const PATTERN_NAME: &str = "name";
+
+#[derive(Parser, Debug)]
+pub struct Args {
+    /// The name of the hwmon path to use.
+    #[arg(short, long)]
+    name: String,
+
+    /// The label of the hwmon path to use.
+    #[arg(short, long)]
+    label: String,
+
+    /// The link to create for the specified hwmon path.
+    #[arg(short = 'p', long)]
+    link_path: String,
+}
 
 fn is_temp_input(entry: &DirEntry) -> bool {
     if let Some(name) = entry.file_name().to_str() {
-        // name.contains()
-        let re = Regex::new(r"temp(.+)_input").unwrap();
-        return re.is_match(name);
+        return name.contains(PATTERN_TEMP) && name.contains(PATTERN_INPUT);
     }
     false
 }
 
 fn main() -> io::Result<()> {
-    for entry in WalkDir::new(HWMON_DIR)
+    let args = Args::parse();
+
+    for temp_input in WalkDir::new(HWMON_DIR)
         .follow_links(true)
         .max_depth(2)
         .into_iter()
         .filter_map(|r| r.ok())
         .filter(is_temp_input)
     {
-        println!("{}", entry.path().display());
+        let path = temp_input.path();
+        if let Some(dir) = path.parent() {
+            let name = fs::read_to_string(dir.join(PATTERN_NAME))?;
+            if name.trim() != args.name {
+                continue;
+            }
+
+            let path_str = path.to_str().unwrap();
+            if let Some(index) = path_str.rfind(PATTERN_INPUT) {
+                let mut label_path = path_str.to_string();
+                label_path.replace_range(index.., PATTERN_LABEL);
+
+                if let Ok(label) = fs::read_to_string(label_path) {
+                    if label.trim() == args.label {
+                        let _ = fs::remove_file(&args.link_path);
+                        symlink(temp_input.path(), &args.link_path)?;
+                    }
+                }
+            }
+        }
     }
 
     Ok(())
