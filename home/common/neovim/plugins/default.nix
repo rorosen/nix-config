36b1c6275f477762a3a5efdb92d04d0de6827c32
Robert Rose <robert.rose@mailbox.org>
2024-08-04 19:42:09 +0200

    nvim: add completion

--- home/common/neovim/plugins/default.nix
+++ home/common/neovim/plugins/default.nix
@@ -11,9 +11,40 @@
     ./treesitter.nix
     ./lsp.nix
     ./efm.nix
+    ./todo.nix
+    ./completion.nix
   ];
 
   programs.nixvim = {
     colorschemes.gruvbox.enable = true;
+    plugins = {
+      nvim-autopairs.enable = true;
+      nvim-colorizer = {
+        enable = true;
+        userDefaultOptions.names = false;
+      };
+
+      gitsigns = {
+        enable = true;
+        settings.signs = {
+          add.text = "+";
+          change.text = "~";
+        };
+      };
+
+      trim = {
+        enable = true;
+        settings = {
+          highlight = true;
+          ft_blocklist = [
+            "checkhealth"
+            "floaterm"
+            "lspinfo"
+            "neo-tree"
+            "TelescopePrompt"
+          ];
+        };
+      };
+    };
   };
 }
