8f7b8738296d69d459f38a604f309d97fcc8c725
Robert Rose <robert.rose@mailbox.org>
2024-08-13 00:18:00 +0200

    nvim: try to add format command

--- home/common/neovim/plugins/conform.nix
+++ home/common/neovim/plugins/conform.nix
@@ -6,18 +6,36 @@
     yamlfmt
     shfmt
   ];
-  programs.nixvim.plugins.conform-nvim = {
-    enable = true;
-    formatOnSave.lspFallback = true;
-    notifyOnError = true;
-    formattersByFt = {
-      nix = [ "nixfmt" ];
-      markdown = [ "prettierd" ];
-      json = [ "prettierd" ];
-      yaml = [ "yamlfmt" ];
-      css = [ "prettierd" ];
-      html = [ "prettierd" ];
-      sh = [ "shfmt" ];
+  programs.nixvim = {
+    plugins.conform-nvim = {
+      enable = true;
+      formatOnSave.lspFallback = true;
+      notifyOnError = true;
+      formattersByFt = {
+        nix = [ "nixfmt" ];
+        markdown = [ "prettierd" ];
+        json = [ "prettierd" ];
+        yaml = [ "yamlfmt" ];
+        css = [ "prettierd" ];
+        html = [ "prettierd" ];
+        sh = [ "shfmt" ];
+      };
+    };
+    userCommands.Format = {
+      range = true;
+      command = ''
+        function(args)
+          local range = nil
+          if args.count ~= -1 then
+            local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+            range = {
+              start = { args.line1, 0 },
+              ["end"] = { args.line2, end_line:len() },
+            }
+          end
+          require("conform").format({ async = true, lsp_format = "fallback", range = range })
+        end
+      '';
     };
   };
 }
