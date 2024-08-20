b11ad7e4e4681fb25b99fb2a103e47c0afd70acb
Robert Rose <robert.rose@mailbox.org>
2024-08-20 15:58:32 +0200

    nvim: remove oil and neo-tree

--- home/common/neovim/plugins/default.nix
+++ home/common/neovim/plugins/default.nix
@@ -1,7 +1,6 @@
 {
   imports = [
     ./lualine.nix
-    ./neo-tree.nix
     ./markdown-preview.nix
     ./barbar.nix
     ./floaterm.nix
@@ -10,7 +9,6 @@
     ./telescope.nix
     ./treesitter.nix
     ./lsp.nix
-    # ./efm.nix
     ./todo.nix
     ./comments.nix
     ./conform.nix
