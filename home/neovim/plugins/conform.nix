89a9455dbc2920b294d544135aa1d7679ad39ac3
Robert Rose <robert.rose@mailbox.org>
2024-10-02 13:52:57 +0200

    nvim: reconfigure conform formatters

--- home/neovim/plugins/conform.nix
+++ home/neovim/plugins/conform.nix
@@ -1,26 +1,50 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    nixfmt-rfc-style
-    prettierd
-    yamlfmt
-    shfmt
-    taplo
-  ];
   programs.nixvim = {
     plugins.conform-nvim = {
       enable = true;
       settings = {
         notify_on_error = true;
         formatters_by_ft = {
+          bash = [ "shfmt" ];
+          json = [ "jq" ];
+          markdown = [ "deno_fmt" ];
           nix = [ "nixfmt" ];
-          markdown = [ "prettierd" ];
-          json = [ "prettierd" ];
-          yaml = [ "yamlfmt" ];
-          css = [ "prettierd" ];
-          html = [ "prettierd" ];
           sh = [ "shfmt" ];
           toml = [ "taplo" ];
+          yaml = [ "yamlfmt" ];
+        };
+        formatters = {
+          deno_fmt = {
+            command = lib.getExe pkgs.deno;
+            args.__raw = ''
+              function(self, ctx)
+                local extensions = {
+                  javascript = "js",
+                  javascriptreact = "jsx",
+                  json = "json",
+                  jsonc = "jsonc",
+                  markdown = "md",
+                  esmodule = "mjs",
+                  typescript = "ts",
+                  typescriptreact = "tsx",
+                }
+                return {
+                  "fmt",
+                  "-",
+                  "--ext",
+                  extensions[vim.bo[ctx.buf].filetype],
+                  "--line-width",
+                  "100",
+                }
+              end
+            '';
+          };
+          jq.command = lib.getExe pkgs.jq;
+          nixfmt.command = lib.getExe pkgs.nixfmt-rfc-style;
+          shfmt.command = lib.getExe pkgs.shfmt;
+          taplo.command = lib.getExe pkgs.taplo;
+          yamlfmt.command = lib.getExe pkgs.yamlfmt;
         };
       };
     };
